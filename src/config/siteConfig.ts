776987989e789e82fcd25d9ed6e2bf8a353e2b4c
Lemon-Bear34 <3029871866@qq.com>
2026-08-31 11:28:50 +0800

    tmdb Init

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -266,12 +266,12 @@ export const siteConfig: SiteConfig = {
 			uid: "514681736",
 		},
 		// TMDB 配置（可选，需要翻墙）
-		// tmdb: {
-		//   // TMDB API 密钥
-		//   apiKey: "your_tmdb_api_key",
-		//   // TMDB 列表 ID
-		//   listId: "your_list_id",
-		// },
+		tmdb: {
+		  // TMDB API 密钥
+		  apiKey: "8965b0cd0361ebbc0872e3648d692699",
+		  // TMDB 列表 ID
+		  listId: "8690709",
+		},
 	},
 
 	// 分页配置
